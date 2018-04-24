3685d6d1e4e1558c02492faeed799bd20e910675
pranesh818 <38650881+pranesh818@users.noreply.github.com>
2018-04-24 14:47:26 +0530

    Create alphabet.c

---
+++ codekata/alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+      char c;
+     printf("Input: ");
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("\n Output");
+        printf("Yes");
+    else
+        printf("\n Output");
+        printf("No");
+
+    return 0;
+}
