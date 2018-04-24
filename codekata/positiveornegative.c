5d8b71503598f5255de8bf5d1d322b9b1bdd3e97
pranesh818 <38650881+pranesh818@users.noreply.github.com>
2018-04-24 12:31:06 +0530

    Update positiveornegative.c

--- codekata/positiveornegative.c
+++ codekata/positiveornegative.c
@@ -4,11 +4,11 @@ void main()
   int a; // declaration of variable
   printf ("\n enter the number:");  
   scanf("%d",&a);
-  if (a>0&&a<100000)  //condition for checking positive or not
+  if (a>=1&&a<100000)  //condition for checking positive or not
     {
         printf("\nThe number is positive");
     }
-  else if(a=0)      // condition for checking equal to zero
+  else if(a==0)      // condition for checking equal to zero
     {
       printf("\n the number is zero");
     }
